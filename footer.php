4ebab9d14033029b4a3a08ac244a7858d4809d72
Geosmi <geosmi24@gmail.com>
2018-05-21 02:08:21 +0300

    Add files via upload
    reviews kai edited mamou apo katw

--- footer.php
+++ footer.php
@@ -4,32 +4,32 @@
     <div class="one_quarter first">
       <h6 class="title">Company Details</h6>
       <address class="btmspace-15">
-      Company Name<br>
-      Street Name &amp; Number<br>
-      Town<br>
-      Postcode/Zip
+      AgroAuction<br>
+      Athinas &amp; 3<br>
+      Kozani<br>
+      50100
       </address>
       <ul class="nospace">
         <li class="btmspace-10"><span class="fa fa-phone"></span> +00 (123) 456 7890</li>
-        <li><span class="fa fa-envelope-o"></span> info@domain.com</li>
+        <li><span class="fa fa-envelope-o"></span> email@email.com</li>
       </ul>
     </div>
     <div class="one_quarter">
-      <h6 class="title">From The Blog</h6>
+      <h6 class="title">adadadadasdads</h6>
       <article>
-        <h2 class="nospace font-x1"><a href="#">Maecenas pretium</a></h2>
+        <h2 class="nospace font-x1"><a href="#">random words </a></h2>
         <time class="smallfont" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
-        <p>Vestibulumaccumsan egestibulum eu justo convallis augue estas aenean elit intesque sed.</p>
+        <p>random words random words random words random words random words random words </p>
       </article>
     </div>
     <div class="one_quarter">
-      <h6 class="title">Sed consectetur enim</h6>
+      <h6 class="title">random words random words </h6>
       <ul class="nospace linklist">
-        <li><a href="#">Nullam id est condimen</a></li>
-        <li><a href="#">Praesent eget purus ut</a></li>
-        <li><a href="#">Ut quis augue quis neque</a></li>
-        <li><a href="#">Quisque varius erat sed</a></li>
-        <li><a href="#">Fusce quis dolor et</a></li>
+        <li><a href="#">random words random words </a></li>
+        <li><a href="#">Prandom words random words </a></li>
+        <li><a href="#">random words random words </a></li>
+        <li><a href="#">random words random words </a></li>
+        <li><a href="#">random words random words </a></li>
       </ul>
     </div>
     <div class="one_quarter">
@@ -49,25 +49,25 @@
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
-<div class="wrapper row6">
+<!--<div class="wrapper row6">
   <div id="copyright" class="clear"> 
     <!-- ################################################################################################ -->
-    <p class="fl_left">Copyright &copy; 2015 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <!--<p class="fl_left">Copyright &copy; 2015 - All Rights Reserved - <a href="#">Domain Name</a></p>
     <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
     <!-- ################################################################################################ -->
-  </div>
+  <!--</div>
 </div>
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
-<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!--<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
 <!-- JAVASCRIPTS -->
-<script src="layout/scripts/jquery.min.js"></script>
+<!--<script src="layout/scripts/jquery.min.js"></script>
 <script src="layout/scripts/jquery.backtotop.js"></script>
 <script src="layout/scripts/jquery.mobilemenu.js"></script>
 <script src="layout/scripts/jquery.flexslider-min.js"></script>
 <!-- IE9 Placeholder Support -->
-<script src="layout/scripts/jquery.placeholder.min.js"></script>
+<!--script src="layout/scripts/jquery.placeholder.min.js"></script>
 <!-- / IE9 Placeholder Support -->
 </body>
 <?php include 'login.php'; ?>
