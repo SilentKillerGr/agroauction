d5b4d0ab82c85a1006dfaa45e8170852b5c695a9
Bill Kehagias <b.keh97@hotmail.com>
2018-05-21 18:43:18 +0300

    profile categories

--- profile.php
+++ profile.php
@@ -1,44 +1,25 @@
-<?php include 'header.php'; ?>
-<section id="pageintro" class="clear">
-</section>
-<div class="wrapper row3">
-  <main class="container clear"> 
-    <div class="content three_quarter first"> 
-      <h1>Καλωσήρθατε, <?php echo($_SESSION['loggedin']); ?></h1>
-      <p>
-		Η AgroAuction είναι εδώ για να κάνει τις πωλήσεις σας ευκολότερες. Επιλέξτε "Νέα Δημοπρασία" για να ξεκινήσετε μια νέα,
-		ή "Οι Δημοπρασίες μου" για να ανατρέξετε στις παλαιότερες δημοπρασίες σας. Με την επιλογή της νέας Δημοπρασίας, δηλώνοντας
-		το προϊόν, αυτόματα οι έμποροι ανταγωνίζονται γι αυτό. Εσείς δεν έχετε παρά να στείλετε το προϊόν στην διεύθυνση του 
-		νικητή, η οποία θα σας αποσταλλεί αυτομάτως.
-
-		Μπορείτε να πωλήσετε διαφόρων ειδών προϊόντα, τα οποία υποστηρίζει το σάιτ μας. Η βραβευμένη μας πλατφόρμα κάνει τη
-		διαδικασία εύκολη για εσάς. Σε περίπτωση προβλήματος, ή σε περίπτωση που έχετε κάποια πρόταση για μας, μπορείτε να
-		επικοινωνήσετε μαζί μας ανά πάσα στιγμή.
-
-		Έχετε υπόψιν σας ότι οι δημοπρασίες πρέπει να περιλαμβάνουν μία σύντομη περιγραφή του προϊόντος, καθώς και φωτογραφίες του,
-		ώστε να μπορέσουν να αξιολογηθούν από τους εμπόρους.
-
-		Σας ευχαριστούμε θερμά για την προτίμηση και την εμπιστοσύνη σας.
-
-		  Η ομάδα της AgroAuction.</p></br>
-      <h2>Τρέχουσες Δημοπρασίες</h2>
-    </div>
-    <div class="sidebar one_quarter"> 
-      <div class="sdb_holder">
-        <h6>Τα στοιχεία μου</h6>
-        <address>
-        Full Name<br>
-        Address Line 1<br>
-        Address Line 2<br>
-        Town/City<br>
-        Postcode/Zip<br>
-        <br>
-        Tel: xxxx xxxx xxxxxx<br>
-        Email: <a href="#">contact@domain.com</a>
-        </address>
-      </div>
-    </div>
-    <div class="clear"></div>
-  </main>
-</div>
-<?php include 'footer.php' ?>
+<?php 
+	include 'header.php';
+	if(isset($_SESSION['user_type'])){
+		switch($_SESSION['user_type']){
+			case 1:
+				include 'producerprof.php';
+				break;
+			case 2:
+				include 'sellerprof.php';
+				break;
+			default:
+				echo("Παρακαλω συνδεθειτε!");
+				break;
+		}
+	}else{
+		?>
+		<div class="wrapper row3">
+		  	<main class="container clear">
+				<h3>Απαγορεύεται η προσβαση. Παρακαλούμε συνδεθείτε.</h3>
+			</main>
+		</div>
+		<?php
+	}
+	include 'footer.php';
+?>
