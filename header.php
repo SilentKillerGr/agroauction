56d4295a6c2ce1289899a43d9b6b967c0a52360e
Bill Kehagias <b.keh97@hotmail.com>
2018-05-22 13:10:43 +0300

    Sync with remote server, corrected credentials

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
 <?php
 	session_start();
-?>
+?><!DOCTYPE html>
 <html>
 	<head>
 		<title>AgroAuction</title>
--- pages/login_handle.php
+++ pages/login_handle.php
@@ -2,9 +2,9 @@
 	session_start();
 	//DATABASE CONNECTION
 	$dbserver 		= "localhost";
-	$dbusername 	= "root";
-	$dbpassword 	= "";
-	$db 			= "user";
+	$dbusername 	= "id5859464_bkeh97";
+	$dbpassword 	= "asdewq1234!";
+	$db 			= "id5859464_user";
 	
 	//CREATE CONNECTION
 	$conn = new mysqli($dbserver, $dbusername, $dbpassword, $db);
@@ -31,7 +31,6 @@
 	
 	if (password_verify($password, $dbPassword))
 	{
-		echo "The details are correct.";
 		$q2 = "SELECT type FROM users WHERE username = '$username'";
 		$res = mysqli_query($conn, $q2);
 		$ro = mysqli_fetch_assoc($res);
