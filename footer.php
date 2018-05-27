2bf51ff24e3f52270af991494738e2268883aadd
Geosmi <geosmi24@gmail.com>
2018-05-27 16:16:28 +0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,74 +1,48 @@
 <div class="wrapper row5">
+
   <footer id="footer" class="clear"> 
+
     <!-- ################################################################################################ -->
+
     <div class="one_quarter first">
-      <h6 class="title">Company Details</h6>
-      <address class="btmspace-15">
-      AgroAuction<br>
-      Athinas &amp; 3<br>
-      Kozani<br>
-      50100
-      </address>
-      <ul class="nospace">
-        <li class="btmspace-10"><span class="fa fa-phone"></span> +00 (123) 456 7890</li>
-        <li><span class="fa fa-envelope-o"></span> email@email.com</li>
-      </ul>
+
+      <h6 class="title">Test 1</h6>
+
     </div>
+
     <div class="one_quarter">
-      <h6 class="title">adadadadasdads</h6>
-      <article>
-        <h2 class="nospace font-x1"><a href="#">random words </a></h2>
-        <time class="smallfont" datetime="2045-04-06">Friday, 6<sup>th</sup> April 2045</time>
-        <p>random words random words random words random words random words random words </p>
-      </article>
+
+      <h6 class="title">Test 2</h6>
+
     </div>
+
     <div class="one_quarter">
-      <h6 class="title">random words random words </h6>
-      <ul class="nospace linklist">
-        <li><a href="#">random words random words </a></li>
-        <li><a href="#">Prandom words random words </a></li>
-        <li><a href="#">random words random words </a></li>
-        <li><a href="#">random words random words </a></li>
-        <li><a href="#">random words random words </a></li>
-      </ul>
+
+      <h6 class="title">Test 3</h6>
+
     </div>
-    <div class="one_quarter">
-      <h6 class="title">Grab Our Newsletter</h6>
-      <form method="post" action="#">
-        <fieldset>
-          <legend>Newsletter:</legend>
-          <input class="btmspace-15" type="text" value="" placeholder="Name">
-          <input class="btmspace-15" type="text" value="" placeholder="Email">
-          <button class="btn" type="submit" value="submit">Submit</button>
-        </fieldset>
-      </form>
-    </div>
+
     <!-- ################################################################################################ -->
+
   </footer>
+
 </div>
+
 <!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!--<div class="wrapper row6">
-  <div id="copyright" class="clear"> 
-    <!-- ################################################################################################ -->
-    <!--<p class="fl_left">Copyright &copy; 2015 - All Rights Reserved - <a href="#">Domain Name</a></p>
-    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
-    <!-- ################################################################################################ -->
-  <!--</div>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!--<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+
 <!-- JAVASCRIPTS -->
-<!--<script src="layout/scripts/jquery.min.js"></script>
+
+<script src="layout/scripts/jquery.min.js"></script>
+
 <script src="layout/scripts/jquery.backtotop.js"></script>
-<script src="layout/scripts/jquery.mobilemenu.js"></script>
+
 <script src="layout/scripts/jquery.flexslider-min.js"></script>
+
 <!-- IE9 Placeholder Support -->
-<!--script src="layout/scripts/jquery.placeholder.min.js"></script>
-<!-- / IE9 Placeholder Support -->
+
+<script src="layout/scripts/jquery.placeholder.min.js"></script>
 </body>
-<?php include 'login.php'; ?>
 </html>
+<?php //include 'login.php'; ?>
--- header.php
+++ header.php
@@ -1,21 +1,23 @@
-<!DOCTYPE html>
+<?php
+	session_start();
+?><!DOCTYPE html>
 <html>
 	<head>
 		<title>AgroAuction</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 		<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+		<link href="../layout/styles/login.css" rel="stylesheet">
 		<link href="layout/styles/login.css" rel="stylesheet">
 		<script> var __adobewebfontsappname__="dreamweaver" </script>
-		<script src="http://use.edgefonts.net/montserrat:n4:default;source-sans-pro:n2:default.js" type="text/javascript"></script>
-		<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet" type="text/css">
 	</head>
 	<body id="top">
 	<!-- ################################################################################################ -->
 	<!-- ################################################################################################ -->
 	<!-- ################################################################################################ -->
 	<!-- Top Background Image Wrapper -->
-	<div class="row100 bgded" style="background-image:url('images/demo/01.png');">
+	<div class="row100 bgded" style="background-image:url('images/inuse/01.png');">
 	  <div class="wrapper row1 overlay"> 
 		<!-- ################################################################################################ -->
 		<!-- ################################################################################################ -->
@@ -23,36 +25,62 @@
 		<header id="header" class="clear"> 
 		  <!-- ################################################################################################ -->
 		  <div id="logo" class="fl_left">
-			<h1><a href="index.php">AgroAuction</a></h1>
+			<h1><a href="pages/index.php">AgroAuction</a></h1>
 		  </div>
-		  <nav id="mainav" class="fl_right">
-			<ul class="clear">
-			  <li class="active"><a href="index.html">Home</a></li>
-			  <li><a class="drop" href="#">Pages</a>
-				<ul>
-				  <li><a href="pages/gallery.html">Gallery</a></li>
-				  <li><a href="pages/full-width.html">Full Width</a></li>
-				  <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
-				  <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
-				  <li><a href="pages/basic-grid.html">Basic Grid</a></li>
-				</ul>
-			  </li>
-			  <li><a class="drop" href="#">Dropdown</a>
-				<ul>
-				  <li><a href="#">Level 2</a></li>
-				  <li><a class="drop" href="#">Level 2 + Drop</a>
-					<ul>
-					  <li><a href="#">Level 3</a></li>
-					  <li><a href="#">Level 3</a></li>
-					  <li><a href="#">Level 3</a></li>
-					</ul>
-				  </li>
-				  <li><a href="#">Level 2</a></li>
-				</ul>
-			  </li>
-			  <li><a href="#">Link Text</a></li>
-			  <li><a href="#">Link Text</a></li>
-			</ul>
-		  </nav>
+			<nav id="mainav" class="fl_right">
+					<ul class="clear">
+			<?php 
+				if (isset($_SESSION['user_type'])){
+				switch ($_SESSION['user_type'])
+				{
+				  case 1:
+						include 'pages/prodmodals.php';
+					?>
+						<li class="active"><a href="index.php">Αρχικη</a></li>
+					  <li><a class="drop" href="#">Δημοπρασιες</a>
+						<ul>
+						  <li><a href="#">Νέα Δημοπρασία</a></li>
+							<li><a href="#">Οι Δημοπρασίες Μου</a></li>
+						</ul>
+					  </li>
+					  <li><a href="pages/profile.php">Προφιλ</a></li>
+						<li><a href="pages/logout.php">Αποσυνδεση</a></li>
+					<?php
+						
+					break;
+					case 2:
+						?>
+						<li class="active"><a href="index.php">Αρχικη</a></li>
+					  <li><a class="drop" href="#">Δημοπρασιες</a>
+						<ul>
+						  <li><a href="pages/auctions.php">Τρέχουσες Δημοπρασίες</a></li>
+						  <li><a href="#">Ιστορικό Δημοπρασιών</a></li>
+						</ul>
+					  </li>
+					  <li><a href="pages/profile.php">Προφιλ</a></li>
+						<li><a href="pages/logout.php">Αποσυνδεση</a></li>
+						<?php
+						break;
+				  default:
+						?>
+						<li class="active"><a href="index.php">Αρχικη</a></li>
+						<?php
+						break;
+			?>
+					
+					  
+					
+			<?php
+					break;
+				}
+				}else{
+					?>
+						<li class="active"><a href="index.php">Αρχικη</a></li>
+					<?php
+				}
+			?>
+						</ul>
+				  </nav>
+		  
 		  <!-- ################################################################################################ -->
 		</header>
